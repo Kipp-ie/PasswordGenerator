b0a5241712c9bfaa70d84e79acba5dfee3ee9565
Kipp-ie <Jenstheminer@gmail.com>
2023-07-02 12:55:13 +0200

    Added a theme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 import PySimpleGUI as sg
 
+sg.theme('GreenTan')
+
 input_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '@', '?']
 input_special = ['!', '@', '#', '?']
 
